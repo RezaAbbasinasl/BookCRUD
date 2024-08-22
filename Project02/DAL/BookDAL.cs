c87a9c51a66383801c2ab04bba09e0ad160ad480
RezaAbbasnasl <rabbasi.maragheh@gmail.com>
2024-08-22 16:23:05 +0330

    Add project files.

---
+++ Project02/DAL/BookDAL.cs
@@ -0,0 +1,39 @@
+﻿using Project02.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Project02.DAL
+{
+    public class BookDAL
+    {
+
+        private string connectionString = @"SERVER=.\SQL2022;DATABASE = Test;USER ID = sa; PASSWORD = Reza3108; TrustServerCertificate=True;";
+
+        public List<Book> GetAllBook()
+        {
+            List<Book> BookList = new List<Book>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = "SELECT BookId, Titel, Author, Genre, Price, PublishDate FROM Book2";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.CommandType = CommandType.Text;
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Book book = new Book();
+                    book.BookId = (Int32)reader["BookId"];
+                    book.Titel = (string)reader["Titel"];
+                    book.Author = (string)reader["Author"];
+                    book.Genre = (string)reader["Genre"];
+                    book.Price = (decimal)reader["Price"];
+                    book.PublishDate = (DateTime)reader["PublishDate"];
+                    BookList.Add(book);
+                }        
+            };
+            return BookList;
+        }
+    }
+}
---
+++ Project02/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Project02.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Titel  { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
